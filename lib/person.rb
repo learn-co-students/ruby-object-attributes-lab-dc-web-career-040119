68db2863f80205522f61c594c9e53bb31a33fdb2
Angela <malaika.mumba@gmail.com>
2019-04-03 23:56:00 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name #(getter)
+    @name
+  end
+
+  def name=(name)#setter
+    @name = name
+  end
+
+  def job #getter
+    @job
+  end
+
+  def job=(job) #setter
+    @job = job
+  end
+end
